aa78e75223bde99754a84e63d4cf2f58e09032d2
Muneeb Anwar <m.mnb.anwar@gmail.com>
2025-11-24 23:04:45 +0500

    Add Cat class extending Pet with displayInfo method

---
+++ Cat.java
@@ -0,0 +1,26 @@
+package Petstore;
+
+public class Cat extends Pet{
+    private String breed;
+    private String furColor;
+    private boolean isVaccinated;
+
+    Cat(String name, int age, String breed, String furColor, boolean isVaccinated) {
+        super(name, age);
+        this.breed = breed;
+        this.furColor = furColor;
+        this.isVaccinated = isVaccinated;
+    }
+    void displayInfo(){
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("ID: " + getId());
+        System.out.println("Breed: " + breed);
+        System.out.println("FurColor: " + furColor);
+        System.out.println("IsVaccinated: ");
+        if(isVaccinated)
+            System.out.println("Yes");
+        else System.out.println("No");
+    }
+
+}
